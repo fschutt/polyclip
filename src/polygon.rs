08845a6714e0170dfdc5487d08fbe217826c6b30
Felix Schütt <felix.schuett@maps4print.com>
2017-12-17 23:31:52 +0100

    Removed unnecessary number_of_intersections function

--- src/polygon.rs
+++ src/polygon.rs
@@ -88,11 +88,15 @@ impl Polygon {
         self.calculate(other, BoolOpType::Xor)
     }
 
+    // NOTE: The method should be inlined, because this will elide the `operation_type`
+    // tests, which will make the whole thing faster. The function will be inlined four times,
+    // one for each `BoolOpType`.
     #[inline(always)]
     fn calculate(&self, other: &Self, operation_type: BoolOpType)
     -> Option<Vec<Self>>
     {
         use self::BoolOpType::*;
+        use self::EdgeType::*;
         use connector::Connector;
 
         // Trivial result case - either self or other polygon do not exist
@@ -260,7 +264,6 @@ impl Polygon {
                     prev = sweep_line_len;
                 }
 
-                use self::EdgeType::*;
                 match event.edge_type {
                     Normal => {
                         match operation_type {
@@ -460,6 +463,95 @@ fn possible_intersection(e1: &mut SweepEvent, e2: &mut SweepEvent) {
     // Uncomment the following line if overlapping edges are not allowed
     // if e1.polygon_type == e2.polygon_type { return; }
 
+
+    // event = event_vec[event_idx]
+    //
+    // __WARNING__: It is assumed that `event_vec` == `event.other_vec`, i.e. that
+    // event_vec only stores references to itself.
+    //
+    // After this function has run once, this is not the case anymore, some references
+    // will be swapped for the `event_holder`
+    //
+    // This function essentially moves events from the event_vec to the event_holder
+    //
+    // Do not call this function on the same index twice!
+    fn divide_segment<'a>(event_vec: &'a mut Vec<SweepEvent<'a>>,
+                          event_idx: usize,
+                          divide_pt: &'a Point2D,
+                          event_holder: &mut Vec<SweepEvent<'a>>,
+                          eq: &mut BinaryHeap<SweepEvent<'a>>)
+    {
+        let event_clone = event_vec[event_idx].clone();
+        {
+            // push right event
+            event_holder.push(SweepEvent {
+                p: divide_pt,
+                left: false,
+                polygon_type: event_clone.polygon_type,
+                other_vec: event_clone.other_vec,
+                other_idx: event_clone.other_idx,
+                in_out: false,
+                position_in_sweep_line: 0,
+                is_inside: false,
+                edge_type: event_clone.edge_type,
+            });
+
+            // push left event
+            event_holder.push(SweepEvent {
+                p: divide_pt,
+                left: true,
+                polygon_type: event_clone.polygon_type,
+                other_vec: event_clone.other_vec,
+                other_idx: event_clone.other_idx,
+                in_out: false,
+                position_in_sweep_line: 0,
+                is_inside: false,
+                edge_type: unsafe { (*event_clone.other_vec)[event_clone.other_idx].edge_type },
+            });
+        }
+
+        let last = event_holder.len() - 1;
+
+        {
+            let left = event_holder.get_mut(last).unwrap();
+
+            if left.compare(unsafe { &(*event_clone.other_vec)[event_clone.other_idx] }) {
+                // avoid a rounding error. The left event would be processed after the right event
+                // original: (*event.other_vec)[event.other_idx].left = true
+                //
+                // assumes that `event.other_vec` == `event_vec`
+                event_vec[event_clone.other_idx].left = true;
+                left.left = false;
+            }
+        }
+
+        let left  = event_holder.get(last).unwrap();
+        let right = event_holder.get(last - 1).unwrap();
+
+        // original code:
+
+        /*
+            event->other->other = left; // this probably will be invalidate when the event_holder resizes !!!
+            event->other = right;       // in the Rust version, it is replaced by an index
+            eq.push(left);
+            eq.push(right);
+        */
+
+        let event_other_idx = event_vec[event_idx].other_idx;
+
+        // assumes that `event.other_vec` == `event_vec`
+        event_vec[event_other_idx].other_vec = event_holder;
+        event_vec[event_other_idx].other_idx = last;
+
+        event_vec[event_idx].other_vec = event_holder;
+        event_vec[event_idx].other_idx = last - 1;
+
+        eq.push(left.clone());
+        eq.push(right.clone());
+    }
+
+    // end of divide_segment()
+
     // note: this will COPY the point!
     let e1_other_p = other!(e1).p;
     let e2_other_p = other!(e2).p;
@@ -585,114 +677,3 @@ fn possible_intersection(e1: &mut SweepEvent, e2: &mut SweepEvent) {
     divide_segment(other!(sorted_events[3]), sorted_events[2].p);
 */
 }
-
-// event = event_vec[event_idx]
-//
-// __WARNING__: It is assumed that `event_vec` == `event.other_vec`, i.e. that
-// event_vec only stores references to itself.
-//
-// After this function has run once, this is not the case anymore, some references
-// will be swapped for the `event_holder`
-//
-// This function essentially moves events from the event_vec to the event_holder
-//
-// Do not call this function on the same index twice!
-fn divide_segment<'a>(event_vec: &'a mut Vec<SweepEvent<'a>>,
-                      event_idx: usize,
-                      divide_pt: &'a Point2D,
-                      event_holder: &mut Vec<SweepEvent<'a>>,
-                      eq: &mut BinaryHeap<SweepEvent<'a>>)
-{
-    let event_clone = event_vec[event_idx].clone();
-    {
-        // push right event
-        event_holder.push(SweepEvent {
-            p: divide_pt,
-            left: false,
-            polygon_type: event_clone.polygon_type,
-            other_vec: event_clone.other_vec,
-            other_idx: event_clone.other_idx,
-            in_out: false,
-            position_in_sweep_line: 0,
-            is_inside: false,
-            edge_type: event_clone.edge_type,
-        });
-
-        // push left event
-        event_holder.push(SweepEvent {
-            p: divide_pt,
-            left: true,
-            polygon_type: event_clone.polygon_type,
-            other_vec: event_clone.other_vec,
-            other_idx: event_clone.other_idx,
-            in_out: false,
-            position_in_sweep_line: 0,
-            is_inside: false,
-            edge_type: unsafe { (*event_clone.other_vec)[event_clone.other_idx].edge_type },
-        });
-    }
-
-    let last = event_holder.len() - 1;
-
-    {
-        let left = event_holder.get_mut(last).unwrap();
-
-        if left.compare(unsafe { &(*event_clone.other_vec)[event_clone.other_idx] }) {
-            // avoid a rounding error. The left event would be processed after the right event
-            // original: (*event.other_vec)[event.other_idx].left = true
-            //
-            // assumes that `event.other_vec` == `event_vec`
-            event_vec[event_clone.other_idx].left = true;
-            left.left = false;
-        }
-    }
-
-    let left  = event_holder.get(last).unwrap();
-    let right = event_holder.get(last - 1).unwrap();
-
-    // original code:
-
-    /*
-        event->other->other = left; // this probably will be invalidate when the event_holder resizes !!!
-        event->other = right;       // in the Rust version, it is replaced by an index
-        eq.push(left);
-        eq.push(right);
-    */
-
-    let event_other_idx = event_vec[event_idx].other_idx;
-
-    // assumes that `event.other_vec` == `event_vec`
-    event_vec[event_other_idx].other_vec = event_holder;
-    event_vec[event_other_idx].other_idx = last;
-
-    event_vec[event_idx].other_vec = event_holder;
-    event_vec[event_idx].other_idx = last - 1;
-
-    eq.push(left.clone());
-    eq.push(right.clone());
-}
-
-
-/*
-// Not sure what this does or if this is needed
-fn number_of_intersections(u0: fsize, u1: fsize, v0: fsize, v1: fsize, w: &mut [fsize; 2]) -> u8 {
-    if (u1 < v0) || (u0 > v1) {
-        return 0;
-    }
-    if u1 > v0 {
-        if u0 < v1 {
-            w[0] = if u0 < v0 { v0 } else { u0 };
-            w[1] = if u1 > v1 { v1 } else { u1 };
-            return 2;
-        } else {
-            // u0 == v1
-            w[0] = u0;
-            return 1;
-        }
-    } else {
-        // u1 == v0
-        w[0] = u1;
-        return 1;
-    }
-}
-*/
